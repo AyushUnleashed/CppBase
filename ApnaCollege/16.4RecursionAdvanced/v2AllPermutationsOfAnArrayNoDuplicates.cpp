cb8666b8ae55035feeb87675adead566edbf6ee1
AyushUnleashed <myzealouslifeofficial@gmail.com>
2021-11-09 02:47:53 +0530

    FromNewLaptop

---
+++ ApnaCollege/16.4RecursionAdvanced/v2AllPermutationsOfAnArrayNoDuplicates.cpp
@@ -0,0 +1,52 @@
+
+//Question from apna college video
+
+#include<iostream>
+using namespace std;
+
+void printArray(int arr[],int n)
+{
+    cout<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i];
+    }
+}
+
+void swap(int* x, int* y)
+{
+    int z = *x;
+    *x = *y;
+    *y = z;
+}
+ 
+
+void permutationArray(int arr[],int n,int cIdx)
+{
+    if(cIdx == n-1)
+    {
+        printArray(arr,n);
+        return;
+    }
+
+    for(int i=cIdx;i<n;i++)
+    {
+        swap(&arr[i],&arr[cIdx]);
+        permutationArray(arr,n,cIdx+1);
+        swap(&arr[i],&arr[cIdx]);
+    }
+}
+
+int main()
+{
+    //sytem("cls");
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    permutationArray(arr,n,0);
+    return 0;
+}
