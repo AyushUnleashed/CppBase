43a382a07710a33b69572724bc4650dc1e241d11
AyushUnleashed <myzealouslifeofficial@gmail.com>
2022-04-05 20:00:11 +0530

    renamed this file

---
+++ CodingNinja/8LinkedList1Assignment/1FindANodeInLinkedList.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+int findNode(Node *head, int n){
+	 
+    Node* temp =head;
+    int counter=0;
+    while( temp!=NULL)
+    {
+        if(temp->data == n)
+        {
+            return counter;
+        }
+        counter++;
+        temp=temp->next;
+    }
+    return -1;
+}
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newNode = new Node(data);
+		if (head == NULL)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		int val;
+		cin >> val;
+		cout << findNode(head, val) << endl;
+	}
+}
