e5d7570a63b7755e3b5e887bc7fdeca313f85d11
AyushUnleashed <myzealouslifeofficial@gmail.com>
2022-09-12 02:14:27 +0530

    isElementPresentInBST

---
+++ CodingNinja/13BST/1IsElementPresentInBinarySearchTree.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+/*Input: 
+8 5 10 2 6 -1 -1 -1 -1 -1 7 -1 -1
+2
+*/
+
+//Ouptut: true
+template <typename T>
+class BinaryTreeNode {
+   public:
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+    ~BinaryTreeNode() {
+        if (left) delete left;
+        if (right) delete right;
+    }
+};
+
+
+bool searchInBST(BinaryTreeNode<int> *root , int k) {
+	if(root==NULL) return false;
+    
+    if(root->data == k) return true;
+    
+    if(k < root->data)
+    {
+        return searchInBST(root->left,k);
+    }
+    else{
+        return searchInBST(root->right,k);
+    }
+    return false;
+}
+
+
+
+
+BinaryTreeNode<int> *takeInput() {
+    int rootData;
+    cin >> rootData;
+    if (rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int> *> q;
+    q.push(root);
+    while (!q.empty()) {
+        BinaryTreeNode<int> *currentNode = q.front();
+        q.pop();
+        int leftChild, rightChild;
+        cin >> leftChild;
+        if (leftChild != -1) {
+            BinaryTreeNode<int> *leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode->left = leftNode;
+            q.push(leftNode);
+        }
+        cin >> rightChild;
+        if (rightChild != -1) {
+            BinaryTreeNode<int> *rightNode =
+                new BinaryTreeNode<int>(rightChild);
+            currentNode->right = rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+int main() {
+    BinaryTreeNode<int> *root = takeInput();
+    int k;
+    cin >> k;
+    cout << ((searchInBST(root, k)) ? "true" : "false");
+    delete root;
+}
