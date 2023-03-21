58a2a3dea0989725db40e212051fb843839bf227
AyushUnleashed <myzealouslifeofficial@gmail.com>
2023-03-21 13:11:35 +0530

    added comments

--- CodingNinja/StriverRecursion/ImpRecursion/GetAllSubSequences.cpp
+++ CodingNinja/StriverRecursion/ImpRecursion/GetAllSubSequences.cpp
@@ -2,19 +2,24 @@
 #include <vector>
 using namespace std;
 
+//faith: gss(bc) -> [--, -c, b-, bc ];
+//expectation: gss(abc) -> [--, -c, b-, bc, a--, a-c, ab-, abc ];
+// f -> e : gss (abc) -> [gss(bc), a +gss(bc)];
+
 vector<string> gss(string s){
     // write your code here
     vector<string> ans;
 
     if(s.length()==0){
-        ans.push_back("");
+        ans.push_back(""); //gss("") -> [""];
         return ans;
     }
-    ans = gss(s.substr(1));
+    ans = gss(s.substr(1)); //ans = gss(bc) -> [--, -c, b-, bc ];
+
     int n = ans.size();
     for(int i=0;i<n;i++){
-        string temp = s[0]+ans[i];
-        ans.push_back(temp);
+        string temp = s[0]+ans[i]; // we made : [a--, a-c, ab-, abc ];
+        ans.push_back(temp); // & pushed it
     }
     return ans;
 }
