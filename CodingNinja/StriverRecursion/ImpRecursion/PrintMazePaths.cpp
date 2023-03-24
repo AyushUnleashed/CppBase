0c09f3eac606b34300a1a1ee3b995c4117a0e434
AyushUnleashed <myzealouslifeofficial@gmail.com>
2023-03-24 22:27:40 +0530

    print maze paths

---
+++ CodingNinja/StriverRecursion/ImpRecursion/PrintMazePaths.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void printMazePaths(int sr, int sc, int dr, int dc, string psf){
+    
+    if(sr==dr && sc==dc){
+        cout<<psf<<endl;
+        return;
+    }
+    
+    //horizontal move
+    if(sc<dc){
+            printMazePaths(sr,sc+1,dr,dc,psf+"h");
+    }
+
+    //vertical move
+        if(sr<dr){
+            printMazePaths(sr+1,sc,dr,dc,psf+"v");
+    }
+
+}
+
+int main(){
+    int n;
+    int m;
+    cin >> n >> m;
+    printMazePaths(0, 0, n - 1, m - 1, "");
+}
